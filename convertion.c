37f87b2dd10a3d48a6b7bd754b3bab79bf6e6d20
Aurios6 <aurelvr@gmail.com>
2024-03-25 16:10:49 +0100

    CutIpAdress finish

--- convertion.c
+++ convertion.c
@@ -7,16 +7,8 @@
 int verifierFormat(const char *ipAdress){
 
 
-      int sizeIpAdress = charLenght(ipAdress);
-     
-      int sizeIndexArrayDot;
-      int *indexArrayDot = charLocationByIndice(ipAdress, '.', &sizeIndexArrayDot);
-      int lowerBound = 0;
-      int upperBound = indexArrayDot[0]-1;
 
-      int sizeIndexArraySlash;
-      int *indexArraySlash = charLocationByIndice(ipAdress, '/', &sizeIndexArraySlash);
-      
+      int sizeIpIntArray;
      /*
      char* subStr = extractSubString(ipAdress, 0,2);
      printf("SubSTR est : %s\n", subStr);
@@ -29,33 +21,66 @@ int verifierFormat(const char *ipAdress){
             return 0;
       }
       
+      int* arrayIpCut = cutIpV4Adress(ipAdress, &sizeIpIntArray);
+      for(int i = 0; i < sizeIpIntArray; i++){
+            printf("%d ",arrayIpCut[i]);
+      }
+      printf("\n");
+
+      /*
+      if (bitIP < 0 || bitIP > 255){
+            printf("Return 0 first\n");
+            
+      }
+      if (bitIP2 < 0 || bitIP2 > 255){
+            printf("Return 0 last\n");
+      }
+      
+      if (bitIP < 0 || bitIP > 255){
+            printf("Return 0 mid\n");
+      }
+      return 1;
+      */
+     return 1;
+}
+
+int* cutIpV4Adress(const char *ipAdress, int *size)
+{
+      int sizeIpAdress = charLenght(ipAdress);
+     
+      int sizeIndexArrayDot;
+      int *indexArrayDot = charLocationByIndice(ipAdress, '.', &sizeIndexArrayDot);
+      int lowerBound = 0;
+      int upperBound = indexArrayDot[0]-1;
+
+      int sizeIndexArraySlash;
+      int *indexArraySlash = charLocationByIndice(ipAdress, '/', &sizeIndexArraySlash);
+
+      int* arrayIntIp = (int*)malloc((sizeIndexArrayDot + sizeIndexArraySlash) * sizeof(int));
+      int arrayIndice = 0;
+
       for(int i = 0; i < sizeIndexArrayDot; i++){
             char*subStr = extractSubString(ipAdress, lowerBound, upperBound);
             int bitIP = strToInt(subStr);
-            //printf("Str to INT %d\n", bitIP);
-            if (bitIP < 0 || bitIP > 255){
-                  printf("Return 0 first\n");
-                  return 0;
-            }
             lowerBound = indexArrayDot[i]+1;
             upperBound =  indexArrayDot[i+1]-1;
+
+            arrayIntIp[arrayIndice] = bitIP;
+            arrayIndice++;
+
       }
+
       char* subStr = extractSubString(ipAdress,indexArrayDot[sizeIndexArrayDot-1]+1, indexArraySlash[sizeIndexArraySlash-1]-1);
       int bitIP = strToInt(subStr);
       //printf("Str to INT %d\n", bitIP);
-      if (bitIP < 0 || bitIP > 255){
-            printf("Return 0 mid\n");
-            return 0;
-      }
+      arrayIntIp[arrayIndice] = bitIP;
+      arrayIndice++;
 
       char* subStr2 = extractSubString(ipAdress, indexArraySlash[sizeIndexArraySlash-1]+1, sizeIpAdress);
       int bitIP2 = strToInt(subStr2);
       //printf("Str to INT %d\n", bitIP2);
-      if (bitIP2 < 0 || bitIP2 > 255){
-            printf("Return 0 last\n");
-            return 0;
-      }
+      arrayIntIp[arrayIndice] = bitIP2;
 
-      printf("Return 1\n");
-      return 1;
+      *size = arrayIndice+1;
+      return arrayIntIp;
 }
--- main.c
+++ main.c
@@ -5,15 +5,17 @@ int main(){
 
       //affTest();
       const char* ipTest = "145.79.198.6/24";
-      const char* ipTest2 = "175.79.198.675/24";
-      const char* ipTest3 = "145.79.1z8.6/24";
-      const char* ipTest4 = ".../";
 
       printf("%s\n",ipTest);
       verifierFormat(ipTest);
+      /*
+      const char* ipTest2 = "175.79.198.675/24";
+      const char* ipTest3 = "145.79.1z8.6/24";
+      const char* ipTest4 = ".../";
       verifierFormat(ipTest2);
       verifierFormat(ipTest3);
       verifierFormat(ipTest4);
+      */
 
 
       return 0;
