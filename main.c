eaa1b2952cbfee73f3b47a5601cf4473ebb6b7f9
Aurios6 <aurelvr@gmail.com>
2024-03-29 14:10:41 +0100

    verif cut IP

--- main.c
+++ main.c
@@ -5,18 +5,15 @@ int main(){
 
       //affTest();
       const char* ipTest = "145.79.198.6/24";
-
-      printf("%s\n",ipTest);
-      verifierFormat(ipTest);
-      /*
       const char* ipTest2 = "175.79.198.675/24";
       const char* ipTest3 = "145.79.1z8.6/24";
       const char* ipTest4 = ".../";
+
+      printf("%s\n",ipTest);
+      verifierFormat(ipTest);
       verifierFormat(ipTest2);
       verifierFormat(ipTest3);
       verifierFormat(ipTest4);
-      */
-
-
+      
       return 0;
 }
